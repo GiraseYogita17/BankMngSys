f16e6d2fe30d06032e4177039accc278cd869d11
GiraseYogita17 <ygirase972@gmail.com>
2025-01-16 15:20:24 +0530

    Commiting Account.java by Yogita

--- Account.java
+++ Account.java
@@ -8,6 +8,7 @@ public class Account {
 	private String panCardNo;
 	private long mobileNo;
 	private double accountBalance;
+        private String name1;
 
 	public long getAccountNo() {
 		return accountNo;
