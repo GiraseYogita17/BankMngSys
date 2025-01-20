e42a7102e9e8d12b501cdbef6f5be52f3e4c1d88
GiraseYogita17 <ygirase972@gmail.com>
2025-01-20 09:15:07 +0530

    Commit

--- Account.java
+++ Account.java
@@ -8,7 +8,7 @@ public class Account {
 	private String panCardNo;
 	private long mobileNo;
 	private double accountBalance;
-        private String name1;
+        private String name2;
 
 	public long getAccountNo() {
 		return accountNo;
